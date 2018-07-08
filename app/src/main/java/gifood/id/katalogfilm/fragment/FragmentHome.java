f470929a2dbe14d84ae7d51f57e5239593467dba
ardiibas <mistersemar.ab@gmail.com>
2018-07-08 17:24:56 +0700

    add fragment and tab layout

---
+++ app/src/main/java/gifood/id/katalogfilm/fragment/FragmentHome.java
@@ -0,0 +1,118 @@
+package gifood.id.katalogfilm.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gifood.id.katalogfilm.R;
+
+public class FragmentHome extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    private OnFragmentInteractionListener listener;
+
+
+    public static FragmentHome newInstance() {
+        return new FragmentHome();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(
+                R.layout.fragment_home, container, false
+        );
+
+        tabLayout = view.findViewById(R.id.fragment_home_tab);
+        tabLayout.addTab(tabLayout.newTab().setText("A"));
+        tabLayout.addTab(tabLayout.newTab().setText("B"));
+
+        viewPager = view.findViewById(R.id.fragment_home_viewpager);
+
+        viewPager.setAdapter(new PagerAdapter(getFragmentManager(), tabLayout.getTabCount()));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        return view;
+    }
+
+    public class PagerAdapter extends FragmentStatePagerAdapter {
+        int mNumOfTabs;
+
+        public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+            super(fm);
+            this.mNumOfTabs = NumOfTabs;
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+
+            switch (position) {
+                case 0:
+                    return new FramgenSatu();
+
+                case 1:
+                    return new FramgenSatu();
+
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return mNumOfTabs;
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof FragmentHome.OnFragmentInteractionListener) {
+            listener = (FragmentHome.OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+
+    }
+}
--- app/src/main/java/gifood/id/katalogfilm/view/Home.java
+++ app/src/main/java/gifood/id/katalogfilm/view/Home.java
@@ -2,14 +2,19 @@ package gifood.id.katalogfilm.view;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import gifood.id.katalogfilm.R;
+import gifood.id.katalogfilm.fragment.FragmentHome;
 
-public class Home extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class Home extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener,
+        FragmentHome.OnFragmentInteractionListener{
 
     private BottomNavigationView bottomNavigationMenu;
 
@@ -18,16 +23,20 @@ public class Home extends AppCompatActivity implements BottomNavigationView.OnNa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.home_frame, FragmentHome.newInstance());
+        fragmentTransaction.commit();
+
         bottomNavigationMenu = findViewById(R.id.home_bottom_nav);
         bottomNavigationMenu.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
+        Fragment fragment = null;
         switch (item.getItemId()){
             case R.id.menu_home:
-                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                fragment = FragmentHome.newInstance();
                 break;
 
             case R.id.menu_search:
@@ -37,6 +46,12 @@ public class Home extends AppCompatActivity implements BottomNavigationView.OnNa
             case R.id.menu_setting:
                 Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
                 break;
+        }
+
+        if (fragment != null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.home_frame, fragment);
+            fragmentTransaction.commit();
         }
 
         return true;
